ad117cf7debce4d01b3159a30d2af1e7d4e8dab7
ahmed alhomsi <72552700+ahmed-alhomsi@users.noreply.github.com>
2021-06-14 19:54:07 +0300

    changed paragraphs

--- 1 page pitch/javascript/app.js
+++ 1 page pitch/javascript/app.js
@@ -2,7 +2,8 @@ const navtoggle = document.querySelector('.nav-toggle');
 const navbar = document.querySelector('.navbar');
 const afternoonheading = document.querySelector('.goodafternoon-heading');
 const begin = document.querySelector('.begin');
-const lastAnimation = document.querySelector('.scale-and-fade')
+const lastAnimation = document.querySelector('.scale-and-fade');
+const lastSection = document.querySelector('.last-animation-section');
 
 navtoggle.addEventListener('click',()=>{
     navbar.classList.toggle('navbar-open');
@@ -12,3 +13,4 @@ window.setTimeout(()=>{
     begin.classList.add('fadein');
     afternoonheading.classList.add('up');
 }, 800);
+
